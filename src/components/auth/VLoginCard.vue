e4bd0621ae5688cfd326f7cd2e44d41ec348f277
César Conte Pérez <yugurta@gmail.com>
2025-05-06 11:56:24 +0200

    style: Adjust padding and elevation for VLoginCard component

--- src/components/auth/VLoginCard.vue
+++ src/components/auth/VLoginCard.vue
@@ -37,10 +37,10 @@ const { xs } = useDisplay() // Accesses display breakpoints from Vuetify
 </script>
 
 <template>
-  <v-card class="rounded-lg pa-8" color="surface" elevation="2">
+  <v-card class="rounded-lg pa-8" color="surface">
     <v-row>
       <v-col cols="auto">
-        <div class="rounded-pill bg-primary px-8 py-8 d-flex">
+        <div class="rounded-pill bg-primary pa-6 d-flex">
           <template v-if="!xs">
             <h3 class="text-h5 text-weight-bold text-on-primary mr-8">{{ title }}</h3>
           </template>
@@ -69,7 +69,7 @@ const { xs } = useDisplay() // Accesses display breakpoints from Vuetify
           color="primary"
           :href="'https://accounts.google.com/signin/v2/usernamerecovery?continue=https%3A%2F%2Faccounts.google.com%2F&ddm=0&flowEntry=ServiceLogin&flowName=GlifWebSignIn&followup=https%3A%2F%2Faccounts.google.com%2F&ifkv=Ab5oB3opUM5VLl_Gw1DaVxkZJm5NtYwh3AqkS-QMpVSmtRnnHgvjzNVkGpaPhqDJcLWWAPrJCFWt'"
           target="_blank"
-          rounded="lg"
+          rounded
           >Forgot email?</v-btn
         >
         <v-btn
@@ -79,7 +79,7 @@ const { xs } = useDisplay() // Accesses display breakpoints from Vuetify
           color="primary"
           :href="'https://accounts.google.com/v3/signin/challenge/pk/presend?TL=AKeb6mye_XcbyjuJt93WjRp0qje0nO_i5v7UyX_JFxzP_lbLW8gASdjfiGyljtK-&checkConnection=youtube%3A221&checkedDomains=youtube&cid=2&continue=https%3A%2F%2Faccounts.google.com%2F&ddm=0&flowEntry=ServiceLogin&flowName=GlifWebSignIn&followup=https%3A%2F%2Faccounts.google.com%2F&ifkv=Ab5oB3rpnQ7URyFyvLrnfOLuSWuQJJManup-Q_JZriOqIDiekby3KZcuNuA5Iemv-Ve8--jhoFVK3Q&pstMsg=1'"
           target="_blank"
-          rounded="lg"
+          rounded
           >Forgot password?</v-btn
         >
         <v-btn
@@ -87,7 +87,7 @@ const { xs } = useDisplay() // Accesses display breakpoints from Vuetify
           variant="elevated"
           class="text-none text-subtitle-2 px-8"
           @click="userStore.logInWithGoogle('signup')"
-          rounded="lg"
+          rounded
         >
           Sign in
         </v-btn>
